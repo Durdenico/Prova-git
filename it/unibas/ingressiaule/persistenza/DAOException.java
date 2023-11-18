4f9cbb499fb32335fc8f0ee9313e5b9e350367a4
Durdenico <domenicodecarlo18@gmail.com>
2023-11-18 12:46:24 +0100

    Aggiunte lo strato di persistenza

---
+++ it/unibas/ingressiaule/persistenza/DAOException.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Exception.java to edit this template
+ */
+package it.unibas.ingressiaule.persistenza;
+
+/**
+ *
+ * @author Domenico
+ */
+public class DAOException extends Exception {
+
+    /**
+     * Creates a new instance of <code>DAOException</code> without detail
+     * message.
+     */
+    public DAOException() {
+    }
+
+    /**
+     * Constructs an instance of <code>DAOException</code> with the specified
+     * detail message.
+     *
+     * @param msg the detail message.
+     */
+    public DAOException(String msg) {
+        super(msg);
+    }
+    
+    public DAOException(Exception e) {
+        super(e.toString());
+    }
+}
